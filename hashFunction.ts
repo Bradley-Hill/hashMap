6ef16771ca1c552cb354b2ea21dc48a5439bf3ac
Bradley-Hill <brad.michael.hill.com>
2024-01-11 15:52:26 +0100

    first commit?

---
+++ hashFunction.ts
@@ -0,0 +1,14 @@
+export function stringToNumber(key: string): number {
+  let hashCode = 0;
+
+  const primeNumber = 31;
+  for (let i = 0; i < key.length; i++) {
+    hashCode = primeNumber * hashCode + key.charCodeAt(i);
+  }
+
+  return hashCode;
+}
+
+export function hash(key: string): number {
+  return stringToNumber(key);
+}
---
+++ linkedList.ts
@@ -0,0 +1,35 @@
+interface node {
+  content: null | string;
+  nextNode: null | node;
+}
+
+export function createNode(content: string, nextNode: node | null): node {
+  return {
+    content,
+    nextNode,
+  };
+}
+
+interface linkedList {
+  head: null | node;
+  appendValue(this: linkedList, value: string): node;
+}
+
+export function createLinkedList(): linkedList {
+  return {
+    head: null,
+    appendValue(value: string) {
+      let node = createNode(value, null);
+      if (this.head === null) {
+        this.head = node;
+      } else {
+        let currentNode = this.head;
+        while (currentNode.nextNode !== null) {
+          currentNode = currentNode.nextNode;
+        }
+        currentNode.nextNode = node;
+      }
+      return node;
+    },
+  };
+}
